2bc22a097a754f5954cd1fe8fa4376d7330e0645
brunochaves77 <bruno-mg300@hotmail.com>
2023-06-19 22:32:32 -0300

    add navbar and theme context to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { ThemeProvider } from "styled-components";
+import { useState, useEffect } from "react";
+import { darkTheme, lightTheme } from './utils/Themes.js'
+import Navbar from "./components/Navbar";
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <div className="App">
-      Portfolio
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router >
+        <Navbar />
+      </Router>
+    </ThemeProvider>
   );
 }
 
