c120cf38527d555e0408626291a3bfb6d02fcf73
brunochaves77 <bruno-mg300@hotmail.com>
2023-06-21 22:33:10 -0300

    add bio info in a constants file

---
+++ src/data/constants.js
@@ -0,0 +1,15 @@
+export const Bio = {
+    name: "Bruno Chaves",
+    roles: [
+      "Full Stack Developer",
+      "Software Developer",
+      "Programmer",
+    ],
+    description: "Bio concept.",
+    github: "https://github.com/brunochaves77",
+    resume: "https://drive.google.com/file/d/1BmRImM8_f4Y3YfnbvkOzEz_qBKlfIFzK/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/bruno-guilherme-silva-chaves/",
+    twitter: "",
+    insta: "",
+    facebook: "",
+  };
