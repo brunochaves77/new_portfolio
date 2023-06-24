a82343be2c41c569b237eddb0e19a825398bfbc3
brunochaves77 <bruno-mg300@hotmail.com>
2023-06-23 23:40:04 -0300

    add hero section component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import { ThemeProvider } from "styled-components";
 import { useState, useEffect } from "react";
 import { darkTheme, lightTheme } from './utils/Themes.js'
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import styled from "styled-components";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
@@ -11,6 +19,9 @@ function App() {
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Router >
         <Navbar />
+        <Body>
+          <HeroSection />
+        </Body>
       </Router>
     </ThemeProvider>
   );
