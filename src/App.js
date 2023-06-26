96188cc0b8b5b5160495062a7610bd5fdd285967
brunochaves77 <bruno-mg300@hotmail.com>
2023-06-25 23:43:12 -0300

    add wrapper for creating next sections

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ const Body = styled.div`
   width: 100%;
   overflow-x: hidden;
 `
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
@@ -21,6 +26,8 @@ function App() {
         <Navbar />
         <Body>
           <HeroSection />
+          <Wrapper>
+          </Wrapper>
         </Body>
       </Router>
     </ThemeProvider>
